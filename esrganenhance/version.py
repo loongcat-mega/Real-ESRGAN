fed82ccc1d13d1e74df23489ec1dfc36601f220a
qingruixu <qingruixu@tencent.com>
2025-03-21 10:29:41 +0800

    delete opencv

--- esrganenhance/version.py
+++ esrganenhance/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Mar 20 15:53:33 2025
-__version__ = '0.3.6'
-__gitsha__ = '22d4091'
-version_info = (0, 3, 6)
+# TIME: Thu Mar 20 15:57:20 2025
+__version__ = '0.3.7'
+__gitsha__ = 'cd57d67'
+version_info = (0, 3, 7)
